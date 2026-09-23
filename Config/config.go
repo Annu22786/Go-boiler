2d51f57782957086ad546025a15d9f0097d8dc18
Shaikh Anas Nadeem <nazmeen0081@gmail.com>
2026-09-23 15:47:47 +0530

    configuration added

---
+++ Config/config.go
@@ -0,0 +1,71 @@
+package main
+
+import(
+  "os"
+  "string"
+  "...github.com/joho/godotenv v1.5.1 "
+  "github.com/knadh/koan/v2"
+  "github.com/rs/zerolog"
+  "github.com/go-playground/validator"
+)
+
+type Config struct{
+  Primaryconfig Primary
+  Databaseconfig Database
+  RedisConfig Redis
+  Authconfig Auth
+  Serverconfig Server
+}
+
+  type Primary struct{
+  Env string `koanf:"env" Validate:"required"`
+}
+
+type Database struct{
+  Host string `koanf:"database_host" Validate:"required"`
+  Database_port int `koanf:"database_port" Validate:"required"`
+  User string `koanf:"user" Validate:"required"`
+  Password string `koanf:"password" Validate:"required"`
+  Name string `koanf:"name" Validate:"required"`
+}
+
+type Redis struct{
+  Chache_key string `koanf:"chache_redis" Validate:"required"`
+}
+
+type Auth struct{
+  Auth_key string `koanf:"auth" Validate:"required"`
+}
+
+type Server struct{
+  CORS []string `koanf:"cors" Validate:"required"`//in arrays tgere will be multiple frontebd domains name of ours
+  Server_port int `koanf:"server_port" Validate:"required"`
+  Readtimeout int `koanf:"readout" Validate:"required"`
+  Writedata int `koanf:"writedata" Validate:"required"`
+  Idletimeout int `koanf:"idelout" Validate:"required"`
+}
+
+func loadenv() (*config,error) {
+  logger := zerolog.New(zerlog.Consolewriter(Out: os.Stderr)).with().timestamp().logger()
+  
+  k := koanf.New(".")
+  
+  err= k.Load(env.provider("Boilerplate...",".")func (s string) string{ string.Tolower(string.trimprefix(s,"BOILERPLATE..."))})
+ 
+  if err != nil{
+    logger.Fatal().Err(err).Msg("Env is not present")
+  }
+  makeconfig:=&config
+  err = k.unmarshell("",makeconfig)
+  if err!=nil{
+    logger.Fatal().Err(err).Msg("Loading error")
+  }
+
+  Validate:= validator.New()
+  err = Validate.Struct(makeconfig)
+  if err != nil{
+    logger.Fatal().Err(err).Msg("Validation error")
+  }
+  return makeconfig,nil
+  
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/Annu22786
+
+go 1.27.1
+
+require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.2 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/knadh/koanf/v2 v2.3.6 // indirect
+	github.com/leodido/go-urn v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/rs/zerolog v1.35.1 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
